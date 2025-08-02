15e6a435d21ce463dea5c15cbc66580c9805c86b
Chris <363708+christopherwoodall@users.noreply.github.com>
2025-08-01 21:42:43 -0500

    Update train.py

--- projects/twitter/train.py
+++ projects/twitter/train.py
@@ -99,7 +99,7 @@ if __name__ == "__main__":
     set_seed(config.system.seed)
 
     # construct the training dataset
-    text = open("output.txt", "r").read()  # don't worry we won't run out of file handles
+    text = open("input.txt", "r").read()  # don't worry we won't run out of file handles
     train_dataset = CharDataset(config.data, text)
 
     # construct the model
